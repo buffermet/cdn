49e43c703398851bc6c74fe48b1fc804c7e89c41
buffermet <29265684+buffermet@users.noreply.github.com>
2020-09-10 16:39:36 +1000

    Update music-player.js

--- github.com/buffermet/darkmode/websites/youtube.com/music-player.js
+++ github.com/buffermet/darkmode/websites/youtube.com/music-player.js
@@ -4,16 +4,34 @@
 
 */
 
-document.querySelector("#primary-inner #player").style.display = "none";
-
 let lastTitle = "";
 
-setInterval(async()=>{
-  const title = document.querySelector("ytd-playlist-panel-video-renderer[selected] h4").innerText;
+const pollVideoNotification = () => {
+  let title = "";
+
+  let playlistTitleNode = document.querySelector("ytd-playlist-panel-video-renderer[selected] h4");
+  let singleVideoTitleNode = document.querySelectorAll("yt-formatted-string.style-scope.style-scope.ytd-video-primary-info-renderer")[1];
+
+  if (playlistTitleNode) {
+    title = playlistTitleNode.innerText;
+  } else if (singleVideoTitleNode) {
+    title = singleVideoTitleNode.innerText;
+  }
 
   if (title != lastTitle) {
     new Notification(title);
   }
 
   lastTitle = title;
-}, 4000);
+};
+
+const musicPlayer = () => {
+  document.querySelector("#primary-inner #player").style.display = "none";
+  document.querySelectorAll(
+    "ytd-watch-flexy:not([theater]):not([fullscreen]) #primary.ytd-watch-flexy,\
+     ytd-watch-flexy:not([theater]):not([fullscreen]) #secondary.ytd-watch-flexy"
+  ).forEach(node=>{
+    node.style.paddingTop = "0pt";
+  });
+  setInterval(pollVideoNotification, 2500);
+};
