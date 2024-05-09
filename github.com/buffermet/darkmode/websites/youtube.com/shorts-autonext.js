c6ece438472c3e2266b45c4e0e10ade7b738f15d
☸️ <29265684+buffermet@users.noreply.github.com>
2024-05-09 14:51:57 +0200

    Update shorts-autonext.js to allow manual skipping.

--- github.com/buffermet/darkmode/websites/youtube.com/shorts-autonext.js
+++ github.com/buffermet/darkmode/websites/youtube.com/shorts-autonext.js
@@ -2,7 +2,10 @@ let highestSeconds = 0
 
 const rxStripDecimals = /\..*/
 
-const loop = setInterval(() => {
+const nextButton = document.querySelector("button[aria-label='Next video']")
+const prevButton = document.querySelector("button[aria-label='Previous video']")
+
+const loop = () => {
     const video = document.querySelector("#shorts-player > div.html5-video-container > video")
     const seconds = parseInt(video.currentTime.toString().replace(rxStripDecimals, ""))
     if (seconds > highestSeconds) {
@@ -10,6 +13,17 @@ const loop = setInterval(() => {
     } else if (seconds < highestSeconds) {
         highestSeconds = seconds
         video.click()
-        document.querySelector("button[aria-label='Next video']").click()
+        nextButton.click()
     }
-}, 1000)
+}
+
+let loopId = setInterval(loop, 1000)
+
+const resetLoop = () => {
+    clearInterval(loopId)
+    highestSeconds = 0
+    loopId = setInterval(loop, 1000)
+}
+
+nextButton.addEventListener("click", resetLoop)
+prevButton.addEventListener("click", resetLoop)
