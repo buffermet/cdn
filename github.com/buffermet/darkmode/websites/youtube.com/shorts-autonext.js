ebb26a0a42c88e00354ede2496f98147b0d3a6d6
☸️ <29265684+buffermet@users.noreply.github.com>
2024-05-09 15:37:55 +0200

    Update shorts-autonext.js to allow manual skipping with arrow keys.

--- github.com/buffermet/darkmode/websites/youtube.com/shorts-autonext.js
+++ github.com/buffermet/darkmode/websites/youtube.com/shorts-autonext.js
@@ -4,24 +4,34 @@ const nextButton = document.querySelector("button[aria-label='Next video']")
 const prevButton = document.querySelector("button[aria-label='Previous video']")
 
 const loop = () => {
-    const video = document.querySelector("#shorts-player > div.html5-video-container > video")
-    const seconds = parseInt(video.currentTime)
-    if (seconds > highestSeconds) {
-        highestSeconds = seconds
-    } else if (seconds < highestSeconds) {
-        highestSeconds = seconds
-        video.click()
-        nextButton.click()
-    }
+	const video = document.querySelector("#shorts-player > div.html5-video-container > video")
+	const seconds = parseInt(video.currentTime)
+	if (seconds > highestSeconds) {
+		highestSeconds = seconds
+	} else if (seconds < highestSeconds) {
+		highestSeconds = seconds
+		video.click()
+		nextButton.click()
+	}
 }
 
-let loopId = setInterval(loop, 1000)
+let loopId = setInterval(loop, 200)
 
-const resetLoop = () => {
-    clearInterval(loopId)
-    highestSeconds = 0
-    loopId = setInterval(loop, 1000)
+const clickListener = () => {
+	clearInterval(loopId)
+	highestSeconds = 0
+	setTimeout(() => loopId = setInterval(loop, 200), 1000)
 }
 
-nextButton.addEventListener("click", resetLoop)
-prevButton.addEventListener("click", resetLoop)
+const keyDownListener = event => {
+	if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+		clearInterval(loopId)
+		highestSeconds = 0
+		setTimeout(() => loopId = setInterval(loop, 200), 1000)
+	}
+}
+
+nextButton.addEventListener("click", clickListener)
+prevButton.addEventListener("click", clickListener)
+
+globalThis.addEventListener("keydown", keyDownListener)
