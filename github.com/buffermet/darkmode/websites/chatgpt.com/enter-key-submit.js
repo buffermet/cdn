4f5116d9db7a18dfe8a38daba53f9326d123baf6
☸️ <29265684+buffermet@users.noreply.github.com>
2024-12-10 21:47:13 +0100

    Allow shift+enter

--- github.com/buffermet/darkmode/websites/chatgpt.com/enter-key-submit.js
+++ github.com/buffermet/darkmode/websites/chatgpt.com/enter-key-submit.js
@@ -1,5 +1,4 @@
 // Submit the prompt when pressing Enter key like every chat client in the world...
 globalThis.addEventListener("keydown", e => {
-    // of course it renders the button each time when typing................. *facepalm*
-    if (e.key === "Enter") document.querySelector('button[data-testid="send-button"]').click()
+    if (e.key === "Enter" && !e.shiftKey) document.querySelector('button[data-testid="send-button"]').click()
 })
