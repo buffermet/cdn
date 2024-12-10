0b392d658ae6e6c013ac00a1afdc7aa66f7a96a2
☸️ <29265684+buffermet@users.noreply.github.com>
2024-12-10 15:59:07 +0100

    cringe

--- github.com/buffermet/darkmode/websites/chatgpt.com/enter-key-submit.js
+++ github.com/buffermet/darkmode/websites/chatgpt.com/enter-key-submit.js
@@ -1,5 +1,5 @@
 // Submit the prompt when pressing Enter key like every chat client in the world...
-const sendBtn = document.querySelector('button[data-testid="send-button"]')
 globalThis.addEventListener("keydown", e => {
-    if (e.key === "Enter") sendBtn.click()
+    // of course it renders the button each time when typing................. *facepalm*
+    if (e.key === "Enter") document.querySelector('button[data-testid="send-button"]').click()
 })
